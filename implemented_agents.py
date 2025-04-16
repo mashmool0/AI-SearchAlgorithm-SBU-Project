052e388b9cc4faf6dbd7f19f1193671499063e5a
Amir Mashayekhi <ap4games2017@gmail.com>
2025-04-16 21:21:53 +0330

    start AI Project Implementation Of Search Algorithms in a Game (A* UCS BFS DFS )

---
+++ implemented_agents.py
@@ -0,0 +1,21 @@
+from agent import AbstractSearchAgent
+
+
+class BFSAgent(AbstractSearchAgent):
+    def searching(self):
+        ...  # TODO
+
+
+class BiIDDFSAgent(AbstractSearchAgent):
+    def searching(self):
+        ...  # TODO
+
+
+class AStarAgent(AbstractSearchAgent):
+    def searching(self):
+        ...  # TODO
+
+
+class UCSAgent(AbstractSearchAgent):
+    def searching(self):
+        ...  # TODO
---
+++ main.py
@@ -0,0 +1,33 @@
+import time
+
+from env import Env
+from plotting import Plotting
+
+from implemented_agents import BFSAgent, BiIDDFSAgent, AStarAgent, UCSAgent
+
+
+def main():
+    map_name = "default"  # Choose the map file
+    use_random_teleports = False  # Change to True to use random teleports
+    num_pairs = 2  # Number of random teleport gates if enabled
+    FPS = 60  # Frames per second for animation
+
+    start = (5, 5)  # Start position
+    goal = (45, 25)  # Goal position
+    euclidean_cost = False  # True to use Euclidean distance as cost
+
+    environment = Env(map_name, use_random_teleports, num_pairs)
+    agent = BFSAgent(start, goal, environment, euclidean_cost)     # TODO: your agent here
+
+    start_time = time.time()
+    path, visited = agent.searching()
+    end_time = time.time()
+    run_time = end_time - start_time
+    print(f"Search completed in {run_time:.5f} seconds")
+
+    plot = Plotting(start, goal, environment, FPS)
+    plot.animation(path, visited, agent.COST)
+
+
+if __name__ == "__main__":
+    main()
